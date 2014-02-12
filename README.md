35bc278cf0c3d69fcb2b68e8020918ecab58c2e4
Brad <eosrei@gmail.com>
2014-02-11 16:31:23 -0800

    Initial commit with download and MD5

---
+++ README.md
@@ -0,0 +1,9 @@
+Solr 4.6.x multi-core Ubuntu/Debian install.sh
+----------------------------------------------
+
+This scripts performs an automatic install of the current multi-core Apache
+Solr 4.6.x in Tomcat 6 on Ubuntu/Debian. Ideally, it would be updated to
+support 4.x.x as released and Centos/Redhat.
+
+NOT READY FOR USE!
+
---
+++ install.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Solr 4.6.x multi-core Ubuntu/Debian installer
+
+# Copyright 2014 Brad Erickson
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This scripts performs an automatic install of the current multi-core Apache
+# Solr 4.6.x in Tomcat 6 on Ubuntu/Debian. Ideally, it would be updated to
+# support 4.x.x as released and Centos/Redhat.
+
+# Enable error checking
+set -e
+
+echo Installing Apache Solr multi-core
+echo
+# TODO: Check for user is root, otherwise fail.
+# TODO: Check for open port, default 8080
+# TODO: Check for md5sum
+# TODO: Check for curl
+
+# Install Tomcat 6
+# TODO: Tomcat 7 if available.
+apt-get update
+apt-get install -y tomcat6 tomcat6-admin tomcat6-common tomcat6-user
+
+echo Locating an Apache Download Mirror
+# Get the mirror list, display only lines where http is in the content,
+# get the first match result.
+MIRROR=$(curl -s http://www.apache.org/dyn/closer.cgi \
+         | grep '>http' | grep -o -m 1 'http://[^" ]*/' | head -n1)
+echo Using: $MIRROR
+
+echo Get HTML of the lucene/solr directory
+HTML_LUCENE_SOLR=$(curl -s ${MIRROR}lucene/solr/)
+
+# Get the most recent 4.x.x version number.
+SOLR_VERSION=$(echo $HTML_LUCENE_SOLR | grep -o '4\.[^/ ]*' | tail -n1)
+echo Found version: $SOLR_VERSION
+
+# Convert the version string into an array
+ARRAY_SOLR_VERSION=(${SOLR_VERSION//./ })
+
+# Check the minor version
+if [ ${ARRAY_SOLR_VERSION[1]} != 6 ]; then
+  echo ERROR: Found minor version: ${ARRAY_SOLR_VERSION[1]}. Only Solr 4.6.x is supported by this script.
+  exit 1;
+fi
+
+# Construct a filename and download the file to /tmp
+SOLR_FILENAME=solr-$SOLR_VERSION.tgz
+SOLR_FILE_URL=${MIRROR}lucene/solr/$SOLR_VERSION/$SOLR_FILENAME
+echo Downloading: $SOLR_FILE_URL
+curl -o /tmp/$SOLR_FILENAME $SOLR_FILEURL
+
+# Verify the download
+SOLR_MD5_URL=http://www.us.apache.org/dist/lucene/solr/$SOLR_VERSION/$SOLR_FILENAME.md5
+echo Downloading MD5 checksum: $SOLR_MD5_URL
+curl -o /tmp/$SOLR_FILENAME.md5 $SOLR_MD5_URL
+echo Verifying the MD5 checksum
+(cd /tmp; md5sum -c $SOLR_FILENAME.md5)
+
